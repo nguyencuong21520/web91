67acf4225d7c11746d0f1ebef2131a6652d09863
nguyencuong21520 <nguyencuong21520@gmail.com>
2025-10-19 21:32:55 +0700

    Update MongoDB connection string to use placeholder for password
    - Modified the connection string in connectMongo.js to replace the actual password with a placeholder for security reasons.

--- TH/lesson4_MongoDB/src/data/connectMongo.js
+++ TH/lesson4_MongoDB/src/data/connectMongo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectMongo = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://nguyencuong21520:Menu00d1@nvc.bl27lke.mongodb.net/web91"
+      "mongodb+srv://nguyencuong21520:<password>@nvc.bl27lke.mongodb.net/web91"
     );
     console.log("Connected to MongoDB successfully");
   } catch (err) {
